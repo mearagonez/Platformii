791d6b5060e2bc317b8ca6918712fbda7eafcdaf
nightware17 <drm_619@hotmail.com>
2016-03-13 12:23:12 -0700

    Practice_Toolbarii

---
+++ Toolbar_2/app/src/main/java/nightware17/toolbar_2/MainActivity.java
@@ -0,0 +1,57 @@
+package nightware17.toolbar_2;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.action_new:
+                Log.i("ActionBar","Nuevo!");
+                return true;
+            case R.id.action_search:
+                Log.i("ActionBar","Buscar!");
+                return true;
+            case R.id.action_settings:
+                Log.i("ActionBar","Settings!");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
